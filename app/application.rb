aaaf3906edf3a459a5a2a124d3d9ccb1b424a0ac
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-20 20:56:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,25 +1,27 @@
 class Application
+
   @@items = []
 
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-if req.path=="/items"
-  item_name = req.path.split("/items/").last
-  item = @@items.find{|i| i.name == item_name}
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
 
-if item.nil?
-  resp.write "Item not found"
-  resp.status = 400
-else
-  resp.write item.price
-end
-else
-  resp.write "Route not found"
-  resp.status = 404
-end
+      item = @@items.find{|i| i.name == item_name}
 
-resp.finish
-end
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
 end
