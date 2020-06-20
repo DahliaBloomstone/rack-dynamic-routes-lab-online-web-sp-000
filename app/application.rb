47c796942061ce4fad6893324be1ded547d096bd
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-20 20:55:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,15 +10,15 @@ if req.path=="/items"
   item = @@items.find{|i| i.name == item_name}
 
 if item.nil?
-  resp.write "Route not found"
-  resp.status = 404
+  resp.write "Item not found"
+  resp.status = 400
 else
   resp.write item.price
 end
 else
   resp.write "Route not found"
   resp.status = 404
-
+end
 
 resp.finish
 end
